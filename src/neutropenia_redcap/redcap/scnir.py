d143fcebc07749114008b7da6c1edd4a8a1e93c8
Eli Goldner <etgld@posteo.us>
2026-02-02 14:59:14 -0500

    More scaffolding methods for comment building

--- src/neutropenia_redcap/redcap/scnir.py
+++ src/neutropenia_redcap/redcap/scnir.py
@@ -1,10 +1,12 @@
+import datetime
+import functools
 import logging
 from collections.abc import Collection, Iterable
 from dataclasses import dataclass
 from datetime import date
 from functools import lru_cache
 from itertools import chain, islice, repeat
-from typing import cast
+from typing import Any, cast
 
 import polars as pl
 
@@ -65,10 +67,31 @@ def map_variant_type(variant_type: str | None) -> int | None:
 
 
 @dataclass
+@functools.total_ordering
 class TextSource:
     filename: str
     section: str
     sentence: str
+    file_date: datetime.date
+
+    @staticmethod
+    def is_valid_operand(operand: Any) -> bool:
+        return hasattr(operand, "date") and isinstance(operand.date, datetime.date)
+
+    def __lt__(self, other: Any) -> bool:
+        if not TextSource.is_valid_operand(other):
+            return NotImplemented
+        return self.file_date < other.date
+
+    def __gt__(self, other: Any) -> bool:
+        if not TextSource.is_valid_operand(other):
+            return NotImplemented
+        return self.file_date > other.date
+
+    def __eq__(self, other: Any) -> bool:
+        if not TextSource.is_valid_operand(other):
+            return NotImplemented
+        return self.file_date == other.date
 
 
 @dataclass
@@ -95,14 +118,56 @@ class SCNIRVariant:
         # sum_germ_var{variant_index}_pro_{germline_index}
         yield self.syntax_p
         # sum_germ_var{variant_index}_acmg_{germline_index}
-        yield map_variant_type(self.variant_type)
+        yield self.select_variant_type()
         # sum_germ_var{variant_index}_comment_{germline_index}
         yield self.build_comment()
 
     # Heterozygosity in "Clinical and Research Sequencing Summary Form Comments"
     # or variant level comments
     def build_comment(self) -> str:
-        return ""
+        return self.build_mention_summary()
+
+    def build_mention_summary(self) -> str:
+        normalized_syntax_n = (
+            self.syntax_n.lower() if self.syntax_n is not None else "None found"
+        )
+        normalized_syntax_p = (
+            self.syntax_p.lower() if self.syntax_p is not None else "None found"
+        )
+        return (
+            "Mention Summary:\n"
+            f"Gene: {self.gene.upper()} Nucleotide Syntax: {normalized_syntax_n} Protein Syntax: {normalized_syntax_p}\n\n"
+        )
+
+    def select_variant_type(self) -> int | None:
+        model_parsed_variant_type = map_variant_type(self.variant_type)
+        if model_parsed_variant_type is not None:
+            return model_parsed_variant_type
+        return self.select_variant_type_from_sources()
+
+    def select_variant_type_from_sources(self) -> int | None:
+        raise NotImplementedError("Figure this out")
+
+    def build_source_guide(self) -> str:
+        match len(self.text_sources):
+            case 0:
+                raise ValueError("Mention has no text sources")
+            case 1:
+                return SCNIRVariant.build_single_source_guide(
+                    text_source=next(iter(self.text_sources))
+                )
+            case _:
+                return SCNIRVariant.build_multi_source_guide(
+                    text_sources=self.text_sources
+                )
+
+    @staticmethod
+    def build_single_source_guide(text_source: TextSource) -> str:
+        raise NotImplementedError("Figure this out")
+
+    @staticmethod
+    def build_multi_source_guide(text_sources: Collection[TextSource]) -> str:
+        raise NotImplementedError("Figure this out")
 
     @staticmethod
     def blank_row_fragment() -> Iterable[None]:
