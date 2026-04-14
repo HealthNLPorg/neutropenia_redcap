621ebedc08c2b9d7360d6593cdd8962ba93ef091
Eli Goldner <etgld@posteo.us>
2026-04-14 17:26:47 -0400

    Parsing variant types enum

--- src/neutropenia_redcap/utils/interpretation.py
+++ src/neutropenia_redcap/utils/interpretation.py
@@ -1,6 +1,9 @@
+from collections.abc import Collection
+
+from variants import SupportedVariantTypes
 from variants.sources import TextSource
 
-from .filename import parse_date_from_filename
+from .filename import parse_date_from_filename, parse_type_from_filename
 
 
 def attributes_to_text_source(sentence: str, section: str, filename: str) -> TextSource:
@@ -12,16 +15,30 @@ def attributes_to_text_source(sentence: str, section: str, filename: str) -> Tex
     )
 
 
-# def attributes_to_variant_type(corpus: str, filename: str, section: str, valid_corpora: Collection[str] = ("sds", "scnir")) -> REDCapForm:
-#     report_or_note_type = parse_type_from_filename(filename)
-#     if corpus not in valid_corpora:
-#         raise ValueError(f"Corpus {corpus} not in supported corpora {valid_corpora}.")
-#     is_germline = "germ" in section.strip().lower()
-#     is_rapid_heme = "RHP" == report_or_note_type.strip().upper()
-#     match corpus, is_germline, is_rapid_heme:
-#         case "sds", True, _:
-#             return SDSGermlineForm()
-#         case "scnir", True, _:
-#             return SCNIRGermlineForm
-#         case _, False, True:
-#             return SomaticForm
+# Don't worry about whether a section header contains "somatic" since the only case
+# identified is primarily germline, i.e.:
+# RELEVANT Potential Germline with Possible Secondary Somatic Variants||POTENTIAL\s+GERMLINE\s+\(WITH\s+POSSIBLE\s+SECONDARY\s+SOMATIC\)\s+VARIANTS\s+\(SBDS,\s+TERC,\s+TERT,\s+DKC[L1],\s+AND\s+DDX41\s+ONLY\)\*\:\s*
+def attributes_to_variant_type(
+    corpus: str,
+    filename: str,
+    section: str,
+    valid_corpora: Collection[str] = ("sds", "scnir"),
+) -> SupportedVariantTypes:
+    report_or_note_type = parse_type_from_filename(filename)
+    if corpus not in valid_corpora:
+        raise ValueError(f"Corpus {corpus} not in supported corpora {valid_corpora}.")
+    is_germline = "germ" in section.strip().lower()
+    is_rapid_heme = "RHP" == report_or_note_type.strip().upper()
+    match corpus, is_germline, is_rapid_heme:
+        case "sds", True, _:
+            return SupportedVariantTypes.SDS_GERMLINE
+        case "scnir", True, _:
+            return SupportedVariantTypes.SDS_GERMLINE
+        case _, False, True:
+            return SupportedVariantTypes.SOMATIC
+        case "sds", _, False:
+            return SupportedVariantTypes.SDS_GERMLINE
+        case "scnir", _, False:
+            return SupportedVariantTypes.SCNIR_GERMLINE
+        case _:
+            return SupportedVariantTypes.NA
--- src/neutropenia_redcap/variants/__init__.py
+++ src/neutropenia_redcap/variants/__init__.py
@@ -1,5 +1,13 @@
 from abc import ABC
 from dataclasses import dataclass
+from enum import Enum, auto
+
+
+class SupportedVariantTypes(Enum):
+    SCNIR_GERMLINE = 0
+    SDS_GERMLINE = 1
+    SOMATIC = 2
+    NA = auto()
 
 
 @dataclass(eq=True, frozen=True)
